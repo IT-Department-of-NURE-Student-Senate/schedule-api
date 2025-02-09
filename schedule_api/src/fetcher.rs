b01ca2adb3b9bfb69b268fc567877fb7f9f112b2
Sytnyk Yehor <pencelheimer@proton.me>
2025-02-09 21:18:13 +0200

    feat: test implementation of first route scrapper

--- schedule_api/src/fetcher.rs
+++ schedule_api/src/fetcher.rs
@@ -1,3 +1,7 @@
+use log::{error, info};
+
+use crate::models::{Root, University};
+
 #[derive(Debug)]
 pub struct Fetcher {
     base_url: String,
@@ -15,9 +19,31 @@ impl Fetcher {
     }
 
     pub async fn fetch_groups(&self) -> Result<University, Error> {
+        let response = self
+            .client
+            .get(format!("{}/{}", self.base_url, "P_API_GROUP_JSON"))
+            .send()
+            .await?;
+
+        let bytes = response.bytes().await?;
+        let (decoded, _, _) = encoding_rs::WINDOWS_1251.decode(&bytes); // Convert from CP1251 to UTF-8
+
+        info!("Decoded {} bytes", bytes.len());
+
+        let root: Root = serde_json::from_str(&decoded).map_err(|e| {
+            error!("JSON parsing error: {}", e);
+            e
+        })?;
+
+        Ok(root.university)
     }
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error),
+
+    #[error(transparent)]
+    SerdeJsonError(#[from] serde_json::Error),
 }
--- schedule_api/src/main.rs
+++ schedule_api/src/main.rs
@@ -11,6 +11,9 @@ async fn main() -> Result<(), Error> {
     env_logger::init();
 
     let fetcher = Fetcher::new("http://cist.nure.ua/ias/app/tt")?;
+    let uni = fetcher.fetch_groups().await.unwrap();
+
+    println!("{:#?}", uni);
 
     Ok(())
 }
